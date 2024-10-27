062615ab4edbc4fa6c5c37b12d8cf6ea35bb6364
Zixin Wang <137997172+EveningStudy@users.noreply.github.com>
2024-10-27 20:00:36 +0800

    modify app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
 
 model_path = './utils/craft_mlt_25k.pth'
 
-
+# Manual processing routes
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
     if request.method == 'POST':
@@ -43,7 +43,6 @@ def upload_image():
             color_hex = request.form.get('color', '#000000')
             color_rgb = tuple(int(color_hex[i:i + 2], 16) for i in (1, 3, 5))  # 转换为RGB
             image = cv2.imread(file_path)
-            # image_with_boxes = draw_boxes(file_path, detected_boxes)
             processed_path = os.path.join(app.config['PROCESSED_FOLDER'], 'boxed_' + filename)
             cv2.imwrite(processed_path, image)
 
@@ -52,27 +51,16 @@ def upload_image():
 
             action = request.form.get('action')
             if action == 'automatic':
-                if mosaic_type == 'ColorBlock':
-                    image = mosaicType.ColorBlock(file_path, detected_boxes, color=color_rgb)
-                else:
-                    image = getattr(mosaicType, mosaic_type)(file_path, detected_boxes)
-
-                processed_image_path = os.path.join(app.config['PROCESSED_FOLDER'], 'processed_' + filename)
-                cv2.imwrite(processed_image_path, image)
-                return redirect(url_for('display_image', filename='processed_' + filename))
+                return redirect(url_for('automatic_mosaic', filename=filename, mosaic_type=mosaic_type, color=color_hex))
             elif action == 'manual':
                 return redirect(url_for('select_text_region', filename='boxed_' + filename, mosaic_type=mosaic_type))
 
     return render_template('index.html')
 
-
 @app.route('/select/<filename>', methods=['GET'])
 def select_text_region(filename):
     mosaic_type = request.args.get('mosaic_type', 'GaussianBlur')
     return render_template('select.html', filename=filename, mosaic_type=mosaic_type)
-
-
-
 
 @app.route('/mosaic', methods=['POST'])
 def mosaic_text_region():
@@ -107,16 +95,31 @@ def mosaic_text_region():
             break
     return jsonify({'success': True})
 
-
 @app.route('/save_manual_image/<filename>', methods=['GET'])
 def save_manual_image(filename):
     processed_path = os.path.join(app.config['PROCESSED_FOLDER'], filename)
-
-    # 将图片发送给用户下载
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename, as_attachment=True)
 
+# Automatic processing routes
+@app.route('/automatic/<filename>', methods=['GET', 'POST'])
+def automatic_mosaic(filename):
+    mosaic_type = request.args.get('mosaic_type', 'GaussianBlur')
+    color_hex = request.args.get('color', '#000000')
+    color_rgb = tuple(int(color_hex[i:i + 2], 16) for i in (1, 3, 5))
 
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    detected_boxes = np.load(os.path.join(app.config['PROCESSED_FOLDER'], 'boxes_' + filename + '.npy'), allow_pickle=True)
 
+    if mosaic_type == 'ColorBlock':
+        image = mosaicType.ColorBlock(file_path, detected_boxes, color=color_rgb)
+    else:
+        image = getattr(mosaicType, mosaic_type)(file_path, detected_boxes)
+
+    processed_image_path = os.path.join(app.config['PROCESSED_FOLDER'], 'processed_' + filename)
+    cv2.imwrite(processed_image_path, image)
+    return redirect(url_for('display_image', filename='processed_' + filename))
+
+# Utility routes
 @app.route('/display/<filename>', methods=['GET', 'POST'])
 def display_image(filename):
     if request.method == 'POST':
@@ -124,7 +127,6 @@ def display_image(filename):
         processed_path = os.path.join(app.config['PROCESSED_FOLDER'], filename)
 
         if action == 'save':
-            # Download the processed image
             return send_from_directory(app.config['PROCESSED_FOLDER'], filename, as_attachment=True)
         elif action == 'discard':
             os.remove(processed_path)
@@ -134,16 +136,13 @@ def display_image(filename):
 
     return render_template('display.html', filename=filename)
 
-
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
 
 @app.route('/processed/<filename>')
 def processed_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
